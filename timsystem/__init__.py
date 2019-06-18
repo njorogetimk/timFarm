8ba0a962813d2c65b38902bd4e88a67ebd82099a
Timothy Njoroge <njorogetimk@gmail.com>
2019-06-18 16:56:10 +0300

    Models Configuration

--- timsystem/__init__.py
+++ timsystem/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from timsystem.farm.config import Dev
 
 
 app = Flask(__name__)
 app.config.from_object(Dev)
+db = SQLAlchemy(app)
 
 from timsystem.farm.farmViews import farm
 app.register_blueprint(farm)
+
+
+db.create_all()
--- timsystem/farm/config.py
+++ timsystem/farm/config.py
@@ -1,9 +1,15 @@
 class Base():
     SECRET_KEY = '12343'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class Dev(Base):
     DEBUG = True
+    protocol = 'postgresql://postgres:'
+    password = 'Kinsman.'
+    host = '@localhost'
+    dbase = '/timfarm'
+    SQLALCHEMY_DATABASE_URI = protocol+password+host+dbase
 
 
 class Production(Base):
