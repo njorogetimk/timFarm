46db6a652eaeaf6fac451031d24808989e6fb70e
Timothy Njoroge <njorogetimk@gmail.com>
2019-06-25 18:36:31 +0300

    Add timepicker JavaScript

--- timsystem/static/js/matCustom.js
+++ timsystem/static/js/matCustom.js
@@ -17,3 +17,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.datepicker');
   var instances = M.Datepicker.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.timepicker');
+  var instances = M.Timepicker.init(elems, {});
+});
