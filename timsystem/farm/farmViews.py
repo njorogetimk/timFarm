09ff1f75446f50a0863530cf84e758f6c99793fe
Timothy Njoroge <njorogetimk@gmail.com>
2019-06-25 08:44:29 +0300

    Set individual record pages and routes

--- timsystem/farm/farmViews.py
+++ timsystem/farm/farmViews.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask import session, flash
-from wtforms import Form, StringField, PasswordField, IntegerField, validators
+from wtforms import Form, StringField, PasswordField, TextAreaField, validators
+from wtforms import IntegerField
 from wtforms.fields.html5 import EmailField
 from timsystem.farm.models import Farm, Users, House, Crop
 from timsystem.farm.models import Day, Activities, Harvest, Condition
@@ -138,6 +139,7 @@ def signin():
             return render_template('signin.html')
         else:
             session['farm_name'] = farm_name
+            session['username'] = username
             if user.level == 'Admin':
                 return redirect(url_for('farm.admin', farm_name=farm_name))
             else:
@@ -227,7 +229,7 @@ def disp_crop(farm_name, house_name, crop_no):
         flash('The crop number is not found', 'danger')
         return redirect(url_for('farm.disp_house', house_name=house_name, farm_name=farm_name))
 
-    days = crop.day.all()
+    days = crop.day.order_by('day_no').all()
     daysdata = {}
 
     for day in days:
@@ -240,7 +242,7 @@ def disp_crop(farm_name, house_name, crop_no):
             condt = condt[0]
         if actv:
             actv = actv[0]
-        daysdata[day.day_no] = {
+        daysdata[day.day_serial] = {
             "condition": condt, "harvest": harv, "activities": actv
         }
 
@@ -286,9 +288,10 @@ def add_crop(farm_name, house_name):
         if not crop:
             # Get started on the new crop
             crop = Crop(house_name, crop_name, crop_no, start_date)
-            day_no = '1-'+crop_no
-            day = Day(crop_no, day_no, start_date)
+            day_serial = '1-'+crop_no
+            day = Day(crop_no, day_serial, start_date)
             db.session.add(crop)
+            db.session.add(day)
             db.session.commit()
             flash('New crop %s created' % crop_no, 'success')
             return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
@@ -312,14 +315,161 @@ def add_day(farm_name, house_name, crop_no):
     if not crop:
         flash('The crop %s is not present in this house' % crop_no, 'danger')
         return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
-    date = request.get('date')
-    daycheck = dayGiver(crop.start_date, date)
-    day_no = str(daycheck.day_no())+crop.crop_no
-    day = Day(crop_no, day_no, date)
-    db.session.add(day)
-    db.session.commit()
-    flash('New day % created' % day_no, 'success')
-    return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+    date = request.form.get('date')
+    daycheck = dayGiver.Dates(crop.start_date, date)
+    day_serial = str(daycheck.day_no())+'-'+crop.crop_no
+    get_day = crop.day.filter_by(day_serial=day_serial).first()
+    if not get_day:
+        day = Day(crop_no, day_serial, date)
+        db.session.add(day)
+        db.session.commit()
+        flash('New day %s created' % day_serial, 'success')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+    else:
+        flash('The day %s is present. Update its record' % day_serial, 'success')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+
+
+class ActvForm(Form):
+    description = TextAreaField("Fill in the day's activities", [
+        validators.DataRequired(),
+        validators.length(min=5, max=100)
+    ])
+
+
+class CondtForm(Form):
+    temperature = IntegerField('Temperature in Celcius', [
+        validators.DataRequired()
+    ])
+    humidity = IntegerField('Relative Humidity', [
+        validators.DataRequired()
+    ])
+    time = StringField('Select the time the records were taken', [
+        validators.DataRequired()
+    ])
+
+
+class HarvForm(Form):
+    punnets = IntegerField('Enter the number of punnets harvested',[
+        validators.DataRequired()
+    ])
+
+
+@farm.route('/<farm_name>/<house_name>/<crop_no>/<day_serial>/record', methods=[
+    'POST', 'GET'])
+def record(farm_name, house_name, crop_no, day_serial):
+    hvForm = HarvForm(request.form)
+    cdnForm = CondtForm(request.form)
+    actvForm = ActvForm(request.form)
+    farm = Farm.query.filter_by(farm_name=farm_name).first()
+    if not farm:
+        flash('Please register your farm %s' % farm_name, 'danger')
+        return redirect(url_for('farm.registerFarm'))
+    house = farm.house.filter_by(house_name=house_name).first()
+    if not house:
+        flash('The house %s is not registered' % house_name, 'danger')
+        return redirect(url_for('farm.admin', farm_name=farm_name))
+    crop = house.crop.filter_by(crop_no=crop_no).first()
+    if not crop:
+        flash('The crop %s is not present in this house' % crop_no, 'danger')
+        return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
+    day = crop.day.filter_by(day_serial=day_serial).first()
+    if not day:
+        flash('The day %s is not added' % day_serial, 'danger')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, crop_no=crop_no, house_name=house_name))
+
+    return render_template('record.html', hvForm=hvForm, cdnForm=cdnForm, actvForm=actvForm, crop=crop, day=day)
+
+
+@farm.route('/<farm_name>/<house_name>/<crop_no>/<day_serial>/record/activities', methods=['POST', 'GET'])
+def record_activities(farm_name, house_name, crop_no, day_serial):
+    form = ActvForm(request.form)
+    farm = Farm.query.filter_by(farm_name=farm_name).first()
+    if not farm:
+        flash('Please register your farm %s' % farm_name, 'danger')
+        return redirect(url_for('farm.registerFarm'))
+    house = farm.house.filter_by(house_name=house_name).first()
+    if not house:
+        flash('The house %s is not registered' % house_name, 'danger')
+        return redirect(url_for('farm.admin', farm_name=farm_name))
+    crop = house.crop.filter_by(crop_no=crop_no).first()
+    if not crop:
+        flash('The crop %s is not present in this house' % crop_no, 'danger')
+        return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
+    day = crop.day.filter_by(day_serial=day_serial).first()
+    if not day:
+        flash('The day %s is not added' % day_serial, 'danger')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, crop_no=crop_no, house_name=house_name))
+
+    if request.method == 'POST' and form.validate():
+        description = form.description.data
+        activities = Activities(day.day_serial, description, session['username'])
+        db.session.add(activities)
+        db.session.commit()
+        flash('Success', 'success')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+    return render_template('record_activities.html', form=form, crop=crop, day=day)
+
+@farm.route('/<farm_name>/<house_name>/<crop_no>/<day_serial>/record/condition', methods=['POST', 'GET'])
+def record_condition(farm_name, house_name, crop_no, day_serial):
+    form = CondtForm(request.form)
+    farm = Farm.query.filter_by(farm_name=farm_name).first()
+    if not farm:
+        flash('Please register your farm %s' % farm_name, 'danger')
+        return redirect(url_for('farm.registerFarm'))
+    house = farm.house.filter_by(house_name=house_name).first()
+    if not house:
+        flash('The house %s is not registered' % house_name, 'danger')
+        return redirect(url_for('farm.admin', farm_name=farm_name))
+    crop = house.crop.filter_by(crop_no=crop_no).first()
+    if not crop:
+        flash('The crop %s is not present in this house' % crop_no, 'danger')
+        return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
+    day = crop.day.filter_by(day_serial=day_serial).first()
+    if not day:
+        flash('The day %s is not added' % day_serial, 'danger')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, crop_no=crop_no, house_name=house_name))
+
+    if request.method == 'POST' and form.validate():
+        temperature = form.temperature.data
+        humidity = form.humidity.data
+        time = form.time.data
+        condition = Condition(day.day_serial, temperature, humidity, time, session['username'])
+        db.session.add(condition)
+        db.session.commit()
+        flash('Success', 'success')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+    return render_template('record_condition.html', form=form, crop=crop, day=day)
+
+
+@farm.route('/<farm_name>/<house_name>/<crop_no>/<day_serial>/record/harvest', methods=['POST', 'GET'])
+def record_harvest(farm_name, house_name, crop_no, day_serial):
+    form = HarvForm(request.form)
+    farm = Farm.query.filter_by(farm_name=farm_name).first()
+    if not farm:
+        flash('Please register your farm %s' % farm_name, 'danger')
+        return redirect(url_for('farm.registerFarm'))
+    house = farm.house.filter_by(house_name=house_name).first()
+    if not house:
+        flash('The house %s is not registered' % house_name, 'danger')
+        return redirect(url_for('farm.admin', farm_name=farm_name))
+    crop = house.crop.filter_by(crop_no=crop_no).first()
+    if not crop:
+        flash('The crop %s is not present in this house' % crop_no, 'danger')
+        return redirect(url_for('farm.disp_house', farm_name=farm_name, house_name=house_name))
+    day = crop.day.filter_by(day_serial=day_serial).first()
+    if not day:
+        flash('The day %s is not added' % day_serial, 'danger')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, crop_no=crop_no, house_name=house_name))
+
+    if request.method == 'POST' and form.validate():
+        punnets = form.punnets.data
+        harvest = Harvest(day.day_serial, punnets, session['username'])
+        db.session.add(harvest)
+        db.session.commit()
+        flash('Success', 'success')
+        return redirect(url_for('farm.disp_crop', farm_name=farm_name, house_name=house_name, crop_no=crop_no))
+    return render_template('record_harvest.html', form=form, crop=crop, day=day)
 
 
 # View Users
@@ -353,11 +503,6 @@ User Routes
 @farm.route('/<farm_name>/user-dashboard')
 def user_dashboard(farm_name):
     return render_template('user_dashboard.html')
-
-# Update records
-@farm.route('/<farm_name>/record', methods=['POST', 'GET'])
-def record(farm_name):
-    return render_template('record.html')
 
 
 # Sign out route
